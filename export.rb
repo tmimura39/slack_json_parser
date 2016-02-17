624aa566afb232c1af93249da9be32afc32533a2
Tomohiko Mimura <mito.5525@gmail.com>
2016-02-17 22:08:26 +0900

    Slack-JSON Parser Beta

---
+++ export.rb
@@ -0,0 +1,35 @@
+require 'json'
+require 'erb'
+
+class User
+  attr_reader :id, :name, :image
+
+  def initialize(id, name, image)
+    @id = id
+    @name = name
+    @image = image
+  end
+end
+
+users = []
+open("./input/users.json") do |io|
+  JSON.load(io).each do |user|
+     users << User.new(user["id"], user["name"], user["profile"]["image_192"])
+  end
+end
+
+channels = []
+open("./input/channels.json") do |io|
+  channels = JSON.load(io).map { |channel| channel["name"] }
+end
+
+channels.each do |channel|
+  File.open("./output/#{channel}.html","w") do |file|
+    Dir["./input/#{channel}/*.json"].each do |json|
+      open(json) do |io|
+        messages = JSON.load(io)
+        file.write(ERB.new(File.read("template.erb")).result(binding))
+      end
+    end
+  end
+end
