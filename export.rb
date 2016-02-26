7cba0db4522760c74f3612d45a2567b816168cca
Tomohiko Mimura <mito.5525@gmail.com>
2016-02-26 21:29:01 +0900

    rubocop

--- export.rb
+++ export.rb
@@ -12,23 +12,23 @@ class User
 end
 
 users = []
-open("./input/users.json") do |io|
+open('./input/users.json') do |io|
   JSON.load(io).each do |user|
-     users << User.new(user["id"], user["name"], user["profile"]["image_192"])
+    users << User.new(user['id'], user['name'], user['profile']['image_192'])
   end
 end
 
 channels = []
-open("./input/channels.json") do |io|
-  channels = JSON.load(io).map { |channel| channel["name"] }
+open('./input/channels.json') do |io|
+  channels = JSON.load(io).map { |channel| channel['name'] }
 end
 
 channels.each do |channel|
-  File.open("./output/#{channel}.html","w") do |file|
+  File.open("./output/#{channel}.html", 'w') do |file|
     Dir["./input/#{channel}/*.json"].each do |json|
       open(json) do |io|
         messages = JSON.load(io)
-        file.write(ERB.new(File.read("template.erb")).result(binding))
+        file.write(ERB.new(File.read('template.erb')).result(binding))
       end
     end
   end
